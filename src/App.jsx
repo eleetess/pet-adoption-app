a6c0deae799a8e10845e5a51cae9eaf3805dd8ab
eleetess <ericaleetesseo@gmai.com>
2025-09-03 09:40:43 -0500

    added readme set up vite scss

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        <h1>Pet adoption</h1>
+        </header>
+        <main>
+          </main>
+          <footer></footer>
+    </>
+  )
+}
+
+export default App
